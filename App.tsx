60eb4c6314c93ca6d9730bde3dc7883a814c2e56
relextm19 <relextm19@gmail.com>
2025-10-29 23:54:09 +0100

    fixed the background not filling all the screen on Android and locked the phone in Landscape

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import CardSpriteTest from "./tests/cardSpritesTestView";
 import MovingBackground from "./components/ui/background";
 import * as NavigationBar from "expo-navigation-bar"
+import * as ScreenOrientation from 'expo-screen-orientation';
 import { useEffect } from "react";
 
 export default function App() {
-  useEffect(()=>{
+  useEffect(() => {
+    const lockOrientation = async () => {
+      await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+    };
+    lockOrientation();
     NavigationBar.setVisibilityAsync('hidden');
   })
   return (
-      <MovingBackground />
+    <MovingBackground />
   );
 }
--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -3,20 +3,19 @@ import { View, Image, StyleSheet } from "react-native";
 import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming, Easing } from "react-native-reanimated";
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../../utils/ResponsiveDimensions";
 
-const [IMAGE_WIDTH, IMAGE_HEIGHT] = [2160, 1914];
-
 export default function MovingBackground(){
     const translateX = useSharedValue(0);
 
     translateX.value = withRepeat(
         withTiming(-SCREEN_WIDTH, {
-            duration: 200000,
+            duration: 280000,
             easing: Easing.linear,
         }),
         -1,
     )
     const animatedStyle = useAnimatedStyle(() =>({
         transform: [
+            {translateX: translateX.value}
         ]
     }));
 
@@ -34,8 +33,7 @@ export default function MovingBackground(){
 }
 const styles = StyleSheet.create({
   container: {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
     overflow: "hidden", 
+    flex: 1,
   },
 });
