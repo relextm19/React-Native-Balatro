b1dc0508ae148e098bf63d934a134c6b5691f588
relextm19 <relextm19@gmail.com>
2025-10-30 16:22:37 +0100

    fixed the background scrolling

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -1,39 +1,58 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
-import Animated, { useSharedValue, useAnimatedStyle, withRepeat, withTiming, Easing } from "react-native-reanimated";
+import { View, StyleSheet } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
 import { SCREEN_HEIGHT, SCREEN_WIDTH } from "../../utils/ResponsiveDimensions";
 
-export default function MovingBackground(){
-    const translateX = useSharedValue(0);
+const [IMAGE_WIDTH,IMAGE_HEIGHT] = [2000, 1000];
 
-    translateX.value = withRepeat(
-        withTiming(-SCREEN_WIDTH, {
-            duration: 280000,
-            easing: Easing.linear,
-        }),
-        -1,
-    )
-    const animatedStyle = useAnimatedStyle(() =>({
-        transform: [
-            {translateX: translateX.value}
-        ]
-    }));
+export default function MovingBackground() {
+  const translateX = useSharedValue(0);
 
-    return(
-        <View style={styles.container}>
-            <Animated.Image
-                source={require("../../assets/backgrounds/background1.png")}
-                style={[
-                    { width: SCREEN_WIDTH * 3, height: SCREEN_HEIGHT * 3 }
-                    , animatedStyle]}
-                resizeMode="repeat" // allows tiling
-            />
-        </View>
-    )
+  translateX.value = withRepeat(
+    withTiming(-IMAGE_WIDTH, {
+      duration: 10000,
+      easing: Easing.linear,
+    }),
+    -1,
+    false
+  );
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateX.value }],
+  }));
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.scrollingContainer, animatedStyle]}>
+        <Animated.Image
+          source={require("../../assets/backgrounds/background1.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+        <Animated.Image
+          source={require("../../assets/backgrounds/background1.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </Animated.View>
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
-    overflow: "hidden", 
     flex: 1,
+  },
+  scrollingContainer: {
+    flexDirection: "row",
+  },
+  image: {
+    marginRight: -2
   },
 });
